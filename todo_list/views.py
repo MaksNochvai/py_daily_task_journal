e8a6c3ba187c136d5ddcdc09ab26c2e02cca0fb3
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-07-17 19:36:59 +0300

    Create views for tags & creted all templates for it

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,35 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
 from django.views.generic import ListView
 
-from todo_list.models import Task
+from todo_list.models import Task, Tag
 
 
 class TaskListView(ListView):
     model = Task
+    template_name = "base.html"
     paginate_by = 5
+
+
+class TagListView(ListView):
+    model = Tag
+    paginate_by = 5
+    template_name = "todo_list/tags.html"
+
+
+class TagsCreateView(generic.CreateView):
+    model = Tag
+    fields = ["name"]
+    success_url = reverse_lazy("todo_list:tags")
+
+
+class TagsUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ["name"]
+    success_url = reverse_lazy("todo_list:tags")
+
+
+class TagsDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_list:tags")
